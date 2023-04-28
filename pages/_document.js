caf3ffe03ae940cf7ce73293c8c86197d87d3c92
Pedro Patito <pedro.patito@hotmail.com>
2023-04-28 11:44:20 -0300

    scrollbar y otros

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" className='dark:bg-gray-800'>
       <Head />
-      <body className='dark '>
+      <body className='dark scrollbar scrollbar-thumb-teal-600 scrollbar-rounded-full scrollbar-medium scrollbar-rounded-edge scrollbar-track-[#d1d4d6] '>
         <Main />
         <NextScript />
       </body>
